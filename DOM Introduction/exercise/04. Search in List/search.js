300847a7fa8a068b5c71d1f3b8e350adf420c8c1
danail <d.zahariev@icloud.com>
2022-09-26 17:05:44 +0300

    DOM Exercise

--- DOM Introduction/exercise/04. Search in List/search.js
+++ DOM Introduction/exercise/04. Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+
+    let towns = Array.from(document.querySelectorAll('ul li'));
+    let search = document.getElementById('searchText').value;
+    let matches = 0;
+
+    for (let town of towns) {
+        let text = town.textContent;
+
+        if (text.includes(search)) {
+            town.style.textDecoration = 'underline';
+            town.style.fontWeight = 'bold';
+            matches++;
+        } else {
+            town.style.textDecoration = 'none';
+            town.style.fontWeight = '';
+        }
+    }
+    document.getElementById('result').textContent = `${matches} matches found`;
 }
