fcd339d21a2104f12e83f2ce9ebde137259c5f52
danail <d.zahariev@icloud.com>
2022-09-27 11:48:05 +0300

    DOM Exercise

--- DOM Introduction/exercise/08. Generate Report/report.js
+++ DOM Introduction/exercise/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+
+    let tableRow = document.querySelectorAll('tbody tr');
+    let tableHeader = document.querySelectorAll('thead tr th');
+
+    let result = [];
+
+    for (let i = 0; i < tableRow.length; i++) {
+
+        let tableData = tableRow[i].cells;
+        let temp = {};
+
+        for (let j = 0; j < tableData.length; j++) {
+
+            let infoHeader = tableHeader[j].childNodes;
+            if (infoHeader[1].checked === true) {
+                temp[infoHeader[0].textContent.trim().toLocaleLowerCase()] = tableData[j].textContent;
+            }
+        }
+        result.push(temp);
+    }
+    document.getElementById('output').textContent = JSON.stringify(result);
 }
--- DOM Introduction/exercise/09. Number Convertor/solution.js
+++ DOM Introduction/exercise/09. Number Convertor/solution.js
@@ -1,4 +1,29 @@
 function solve() {
 
-    //TODO...
+    document.querySelector("#container button").addEventListener('click', convert);
+
+    let selectElement = document.getElementById('selectMenuTo');
+
+    let optionHex = document.createElement('option');
+    optionHex.value = 'hexadecimal';
+    optionHex.textContent = 'Hexadecimal';
+
+    let optionBinary = document.createElement('option');
+    optionBinary.value = 'binary';
+    optionBinary.textContent = 'Binary';
+
+    selectElement.appendChild(optionHex);
+    selectElement.appendChild(optionBinary);
+
+    function convert() {
+
+        let result = document.getElementById('result');
+        let input = Number(document.getElementById('input').value);
+
+        if (selectElement.value === 'binary') {
+            result.value = input.toString(2);
+        } else if (selectElement.value === 'hexadecimal') {
+            result.value = input.toString(16).toUpperCase();
+        }
+    }
 }
