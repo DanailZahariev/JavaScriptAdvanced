09f68f7772ec9b7a46181d93930b08a1cd289702
danail <d.zahariev@icloud.com>
2022-10-01 23:14:08 +0300

    DOM Events exercise

--- DOM Manipulations and Events/exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events/exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let buttons = document.querySelectorAll('button');
+    let textArea = document.querySelectorAll('textarea');
+    let encodeInput = textArea[0];
+    let decodeInput = textArea[1];
+
+    buttons[0].addEventListener('click', encodeMessage);
+    buttons[1].addEventListener('click', decodeMessage);
+
+    function decodeMessage(evt) {
+        let parentEl = evt.target.parentElement;
+        let textValue = parentEl.getElementsByTagName('textarea')[0].value;
+        let decode = textValue.split('').map(el => el.charCodeAt(0) - 1);
+        decodeInput.value = String.fromCharCode.apply(0, decode);
+    }
+
+    function encodeMessage(evt) {
+        let parentEl = evt.target.parentElement;
+        let textValue = parentEl.getElementsByTagName('textarea')[0].value;
+        let encode = textValue.split('').map(el => el.charCodeAt(0) + 1);
+        let text = String.fromCharCode.apply(0, encode);
+        encodeInput.value = '';
+        decodeInput.value = text;
+    }
 }
