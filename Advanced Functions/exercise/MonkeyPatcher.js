53d095216fb44ecc4b8a07f701e21161653a2c33
danail <d.zahariev@icloud.com>
2022-10-12 14:46:53 +0300

    update

--- Advanced Functions/exercise/MonkeyPatcher.js
+++ Advanced Functions/exercise/MonkeyPatcher.js
@@ -1,34 +1,43 @@
-// function solution(command) {
-//
-//     if (command === 'upvote') {
-//         return this.upvotes++;
-//     } else if (command === 'downvote') {
-//         return this.downvotes++;
-//     }
-//
-//     let upvotes = this.upvotes;
-//     let downvotes = this.downvotes;
-//
-//     let totalVotes = upvotes + downvotes;
-//     let totalScore = upvotes - downvotes;
-//
-//     function ratings() {
-//         if (totalVotes < 10) {
-//             return 'new';
-//         } else if (upvotes > totalVotes * 0.66) {
-//             return 'hot';
-//         } else if (totalScore >= 0 && totalVotes > 100) {
-//             return 'controversial';
-//         } else if (totalScore < 0) {
-//             return 'unpopular';
-//         } else {
-//             return 'new';
-//         }
-//     }
-//
-//     if (totalVotes > 50) {
-//         let obfuscate = Math.ceil(Math.max(upvotes, downvotes) * 0.25);
-//         return [upvotes + downvotes, downvotes + obfuscate, totalVotes, ratings()];
-//     }
-//     return [upvotes, downvotes, totalScore, ratings.call(this)]
-// }
+function solution(command) {
+
+    let percent = 0;
+    let allUpVote = 0;
+    let allDownVote = 0;
+    let rating = '';
+    let balance = 0;
+
+    if (command === 'upvote') {
+        this.upvotes++;
+    } else if (command === 'downvote') {
+        this.downvotes++;
+    }
+    if (command === 'score') {
+        balance = this.upvotes - this.downvotes;
+        let totalVotes = this.upvotes + this.downvotes;
+
+        if (totalVotes < 10) {
+            rating = 'new';
+        } else if (totalVotes * 0.66 < this.upvotes) {
+            rating = 'hot';
+        } else if (balance >= 0 && totalVotes > 100) {
+            rating = 'controversial';
+        } else if (balance < 0) {
+            rating = 'unpopular';
+        } else {
+            rating = 'new';
+        }
+        percent = Math.ceil(Math.max(this.upvotes, this.downvotes) * 0.25);
+        allUpVote = totalVotes > 50 ? percent + this.upvotes : this.upvotes;
+        allDownVote = totalVotes > 50 ? percent + this.downvotes : this.downvotes;
+    }
+
+    return [allUpVote, allDownVote, balance, rating];
+}
+
+let post = {
+    id: '3',
+    author: 'emil',
+    content: 'wazaaaaa',
+    upvotes: 100,
+    downvotes: 100
+};
